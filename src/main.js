076125bdc3738749aaf7e2b3ca6fed829af63faa
KALASA_IVAN <moikalasa12@gmail.com>
2022-03-28 11:51:14 +0200

    adding a transaction

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ const { Blockchain, Transaction } = require("./blockchain");
 const EC = require("elliptic").ec;
 const ec = new EC("secp256k1");
 
+const myKey = ec.keyFromPrivate(
+  "7e6d3a7cfed9817db5641fe50062cd4235983c0e4e99211ea243845cba826412"
+);
+const myWalletAddress = myKey.getPublic("hex");
+
+const tx1 = new Transaction(myWalletAddress, "public key goes here", 10);
+tx1.signTransaction(myKey);
+i_coin.addTransaction(tx1)
+
 let i_coin = new Blockchain();
 
-i_coin.createTransaction(new Transaction("Address1", "Address2", 100));
-i_coin.createTransaction(new Transaction("Address2", "Address1", 50));
+// i_coin.createTransaction(new Transaction("Address1", "Address2", 100));
+// i_coin.createTransaction(new Transaction("Address2", "Address1", 50));
 
 console.log("\nStarting the miner ...");
 i_coin.minePendingTransactions("Ivans-address");
